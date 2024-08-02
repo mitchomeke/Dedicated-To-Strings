040732e5411666d42bf3217b30dddd61092688b2
Mitchell Chukwueku Omeke <164729363+mitchomeke@users.noreply.github.com>
2024-08-03 01:58:47 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,19 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
+    /**
+     * NUMBER OF WORDS IN A STRING
+     * @param args
+     */
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String str1 = scanner.nextLine();
-        String str2 = scanner.nextLine();
-
-        if (str1.length() == str2.length()){
-            ArrayList<Character> array = new ArrayList<>();
-                for (int i = 0; i < str1.length();i++){
-                    array.add(str1.charAt(i));
-            }
-                for (int i = 0; i < str2.length();i++){
-                    Character c = str2.charAt(i);
-                    if (array.contains(c)){
-                        array.remove(c);
-                    }
-                }
-                if (array.isEmpty()){
-                    System.out.println("It´s an anagram");
-                }
-                else {
-                    System.out.println("It´s not an anagram!");
-                }
+        String input = scanner.nextLine();
+        String[] array = input.split(" ");
+        int i = 0;
+        for (int j = 0; j < array.length;j++){
+            i++;
         }
-        else {
-            System.out.println("Don´t even bother lol!");
-        }
+        System.out.println(i);
     }
 }
