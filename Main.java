c20c7877d82997c8460cc8f605bd848454f327ab
Mitchell Chukwueku Omeke <164729363+mitchomeke@users.noreply.github.com>
2024-08-03 01:57:09 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,25 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
+    /**
+     * REVERSING STRINGS
+     * @param args
+     */
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String str1 = scanner.nextLine();
-        String str2 = scanner.nextLine();
-
-        if (str1.length() == str2.length()){
-            ArrayList<Character> array = new ArrayList<>();
-                for (int i = 0; i < str1.length();i++){
-                    array.add(str1.charAt(i));
-            }
-                for (int i = 0; i < str2.length();i++){
-                    Character c = str2.charAt(i);
-                    if (array.contains(c)){
-                        array.remove(c);
-                    }
-                }
-                if (array.isEmpty()){
-                    System.out.println("It´s an anagram");
-                }
-                else {
-                    System.out.println("It´s not an anagram!");
-                }
+        String input1 = scanner.nextLine();
+        String output = "";
+        String[] array = input1.split("");
+        int i = input1.length()-1;
+        StringBuilder builder = new StringBuilder();
+        while(i >= 0){
+            builder.append(input1.charAt(i));
+            i--;
         }
-        else {
-            System.out.println("Don´t even bother lol!");
-        }
+        output = builder.toString();
+        System.out.println(output);
     }
 }
