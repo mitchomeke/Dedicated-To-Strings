f9fd24691b42fe1add54ca311d0a5cdcc433686d
Mitchell Chukwueku Omeke <164729363+mitchomeke@users.noreply.github.com>
2024-08-03 01:55:07 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,38 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
+    /**
+     * CHECKING IF A WORD´S LENGTH IS EVEN.
+     * @param args
+     */
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String str1 = scanner.nextLine();
-        String str2 = scanner.nextLine();
-
-        if (str1.length() == str2.length()){
-            ArrayList<Character> array = new ArrayList<>();
-                for (int i = 0; i < str1.length();i++){
-                    array.add(str1.charAt(i));
+        String str = scanner.nextLine();
+        String str2;
+        String str3;
+        int j = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                j = i;
             }
-                for (int i = 0; i < str2.length();i++){
-                    Character c = str2.charAt(i);
-                    if (array.contains(c)){
-                        array.remove(c);
-                    }
-                }
-                if (array.isEmpty()){
-                    System.out.println("It´s an anagram");
-                }
-                else {
-                    System.out.println("It´s not an anagram!");
-                }
         }
-        else {
-            System.out.println("Don´t even bother lol!");
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < j; i++) {
+            builder.append(str.charAt(i));
+        }
+        StringBuilder builder1 = new StringBuilder();
+        str2 = builder.toString();
+        while (j < str.length()){
+            builder1.append(str.charAt(j));
+            j++;
+        }
+        str3 = builder1.toString();
+        if (str2.length() % 2 == 0){
+            System.out.println(str2 + " has an even length of words.");
+        }
+        else if (str3.length() % 2 == 0){
+            System.out.println(str3 + " has an even length of words.");
         }
     }
 }
