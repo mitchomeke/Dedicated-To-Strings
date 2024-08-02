0696f583cfe09ba0bf98db1ec03d10f87d8073c4
Mitchell Chukwueku Omeke <164729363+mitchomeke@users.noreply.github.com>
2024-08-03 01:59:59 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,32 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * COMPARING TWO STRINGS
+ */
 public class Main {
-
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String str1 = scanner.nextLine();
-        String str2 = scanner.nextLine();
+        String input = scanner.nextLine();
+        String input2 = scanner.nextLine();
 
-        if (str1.length() == str2.length()){
-            ArrayList<Character> array = new ArrayList<>();
-                for (int i = 0; i < str1.length();i++){
-                    array.add(str1.charAt(i));
+        int j = 0;
+        char c;
+        char b;
+
+        if (input.length() == input2.length()) {
+            for (int i = 0; i < input.length(); i++) {
+                c = input.charAt(i);
+                b = input2.charAt(j);
+                j++;
+                if (c != b) {
+                    System.out.println("Not equal Strings");
+                    break;
+                }
+                else if (i == input.length()-1){
+                    System.out.println("Equal Strings");
+                }
             }
-                for (int i = 0; i < str2.length();i++){
-                    Character c = str2.charAt(i);
-                    if (array.contains(c)){
-                        array.remove(c);
-                    }
-                }
-                if (array.isEmpty()){
-                    System.out.println("It´s an anagram");
-                }
-                else {
-                    System.out.println("It´s not an anagram!");
-                }
-        }
-        else {
-            System.out.println("Don´t even bother lol!");
         }
     }
 }
