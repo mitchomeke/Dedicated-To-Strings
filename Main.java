fdd5d01b800692395b695d466eb4d56c638e4380
Mitchell Chukwueku Omeke <164729363+mitchomeke@users.noreply.github.com>
2024-08-04 16:58:52 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,26 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String str1 = scanner.nextLine();
-        String str2 = scanner.nextLine();
-
-        if (str1.length() == str2.length()){
-            ArrayList<Character> array = new ArrayList<>();
-                for (int i = 0; i < str1.length();i++){
-                    array.add(str1.charAt(i));
+        String input = scanner.nextLine();
+        int wordcount = 0;
+        int vowelcount = 0;
+        int uppercasecount = 0;
+        String[] array = input.split(" ");
+        for (int i = 0 ; i < array.length;i++){
+            wordcount++;
+        }
+        for (Character ch : input.toCharArray()){
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                vowelcount++;
             }
-                for (int i = 0; i < str2.length();i++){
-                    Character c = str2.charAt(i);
-                    if (array.contains(c)){
-                        array.remove(c);
-                    }
-                }
-                if (array.isEmpty()){
-                    System.out.println("It´s an anagram");
-                }
-                else {
-                    System.out.println("It´s not an anagram!");
-                }
+            if (ch >= 65 && ch <=90){
+                uppercasecount++;
+            }
         }
-        else {
-            System.out.println("Don´t even bother lol!");
-        }
+        System.out.println("word count -> " + wordcount);
+        System.out.println("vowel count -> " + vowelcount);
+        System.out.println("uppercase count -> " + uppercasecount);
     }
 }
